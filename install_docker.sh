30b6c077660a4c7db57ba1c73621a937dee2e156
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-02-21 00:38:48 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -8,7 +8,6 @@ if ! command -v docker &> /dev/null; then
     sudo systemctl enable docker
     sudo systemctl start docker
     sudo usermod -aG docker $USER
-    echo "Docker installed. You may need to log out/in for group changes to take effect."
 else
     echo "Docker already installed. Skipping..."
 fi
